d0c81d8a4307698b57018115d8a912fcd0e5b76a
LeeSangYong <lsy3709@gmail.com>
2024-05-14 11:12:03 +0900

    3교시, MVC 구조, 계산기 구현 기본2, get 숫자 입력폼을 반환, post 로직 처리 결과 반환, 콘솔부터 확인

--- Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/calc/InputController.java
+++ Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/calc/InputController.java
@@ -21,7 +21,8 @@ public class InputController extends HttpServlet {
   //  HttpServletResponse resp , 역할 : 서버가 응답시 정보를 담는 상자로 사용함.
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    super.doGet(req, resp);
+    // doGet 재정의해서 사용할려는 의도인데, 이걸 부모꺼 그대로 쓰면, 의도 맞지 않음.
+//    super.doGet(req, resp);
     System.out.println("작업순서1 : InputController 거쳐가기. ");
     // 컨트롤러에서, 해당 입력폼 화면으로 전달하는 코드.
     // 서버에서는, 웹브라우저가 가야할 페이지 정보를 담아 놓았다. 여기에 requestDispatcher
@@ -34,6 +35,18 @@ public class InputController extends HttpServlet {
 
   // post : 무언가 로직을 처리한다.
 //  예 로그인 처리, 계산기 숫자를 처리하는 로직.
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//    super.doPost(req, resp);
+    // 재정의, -> calc 처리한 결과 페이지로 가는게 목적.
+
+    // 입력 폼에서 넘어온 데이터를 받기. 항상 반환이 문자열 타입이다.
+    String num1 = req.getParameter("num1");
+    String num2 = req.getParameter("num2");
+    // 단순 입력 받은 숫자만 출력해보기.
+    System.out.println("num1: " + num1 + " num2: " + num2);
+  }
 }
 
 
--- Sample501JSP/SampleJsp501/src/main/webapp/WEB-INF/calc/input.jsp
+++ Sample501JSP/SampleJsp501/src/main/webapp/WEB-INF/calc/input.jsp
@@ -7,7 +7,8 @@
 <body>
 <%--action 속성의 값으로 서버에서 처리할 서블릿 주소를 입력 하면 됨.--%>
 <%--예제로, input.jsp -> result1.jsp 로 쿼리스트링을 이용해서 , 파라미터 전달 테스트--%>
-
+<%--원래 의도 대로 , 현재 입력폼이 -> 컨트롤러를 먼저가서 확인후 -> 화면--%>
+<%--result1.jsp , 단순 페이지 입니다. 절대 바로 안감.!!--%>
 <form action="result1.jsp" method="post">
   <input type="number" name="num1">
   <input type="number" name="num2">
