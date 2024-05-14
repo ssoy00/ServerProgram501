cd759614fac8a0c01058676e0699f99e9cbb9564
LeeSangYong <lsy3709@gmail.com>
2024-05-14 11:17:53 +0900

    3교시, MVC 구조, get 로 올경우 입력폼이 나오고, post 로 올경우, 처리결과 콘솔이 나옴.

--- Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/calc/InputController.java
+++ Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/calc/InputController.java
@@ -23,7 +23,7 @@ public class InputController extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     // doGet 재정의해서 사용할려는 의도인데, 이걸 부모꺼 그대로 쓰면, 의도 맞지 않음.
 //    super.doGet(req, resp);
-    System.out.println("작업순서1 : InputController 거쳐가기. ");
+    System.out.println("작업순서1 : InputController 거쳐가기. doGet 으로 받기 ");
     // 컨트롤러에서, 해당 입력폼 화면으로 전달하는 코드.
     // 서버에서는, 웹브라우저가 가야할 페이지 정보를 담아 놓았다. 여기에 requestDispatcher
     RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/calc/input.jsp");
@@ -40,7 +40,7 @@ public class InputController extends HttpServlet {
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //    super.doPost(req, resp);
     // 재정의, -> calc 처리한 결과 페이지로 가는게 목적.
-
+    System.out.println("작업순서1 : InputController 거쳐가기. doPost 으로 받기 ");
     // 입력 폼에서 넘어온 데이터를 받기. 항상 반환이 문자열 타입이다.
     String num1 = req.getParameter("num1");
     String num2 = req.getParameter("num2");
