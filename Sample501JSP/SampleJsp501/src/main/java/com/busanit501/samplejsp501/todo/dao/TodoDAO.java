29c2bb22ee433910c5e5c359740f644801d3cf0f
LeeSangYong <lsy3709@gmail.com>
2024-05-17 12:16:15 +0900

    4교시, HikariCP 독립 이용해서, 디비의 현재 시간 가져오는 기능 설정 및 확인.

--- Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/dao/TodoDAO.java
+++ Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/dao/TodoDAO.java
@@ -1,5 +1,9 @@
 package com.busanit501.samplejsp501.todo.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class TodoDAO {
   // 데이터베이스 직접적인 비지니스 로직 처리하는 기능을 만들기.
   // 샘플
@@ -12,7 +16,26 @@ public class TodoDAO {
     // 3가지 방법. 1) try catch finally 2) throws 3) try resources with (자동 close)
     // 결론, 롬복의 @cleanup , 자동으로 반납함.
 
-    return "테스트 조금 있다 변경함. 현재시각으로";
+    // 예시 try resources with (자동 close)
+    String now = null;
+    // 디비 연결해서, 시각 가져오는 코드.
+    try(Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        // sql 전달하는 도구.
+        // String sql = "select now()"
+    PreparedStatement pstmt = connection.prepareStatement("select now()");
+    // 조회한 결과 테이블을 담기위한 임시 테이블
+        // 0 행부터 대기중.
+    ResultSet rs = pstmt.executeQuery();
+    ){
+      // 1행 만 있어서, while 문 없음.
+      rs.next();
+      // 1행의 결과를 가져오기. 문자열
+      now = rs.getString(1);
+    } catch (Exception exception) {
+      exception.printStackTrace();
+    } //원래는 finally 구문으로 close 해야하지만,
+    // try resource , 자동 close 가 포함되어 있다.
+    return now;
   }
 }
 
