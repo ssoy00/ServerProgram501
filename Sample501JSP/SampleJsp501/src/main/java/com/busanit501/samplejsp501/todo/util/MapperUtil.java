4390bb9a69d48f9027b5d34db72e7e9588bc20b2
LeeSangYong <lsy3709@gmail.com>
2024-05-20 14:01:55 +0900

    5교시, 모델맵퍼 기본 설정, enum 타입에 만들기.

--- Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/util/MapperUtil.java
+++ Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/util/MapperUtil.java
@@ -1,6 +1,8 @@
 package com.busanit501.samplejsp501.todo.util;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
 
 public enum MapperUtil {
   INSTANCE;
@@ -16,6 +18,18 @@ public enum MapperUtil {
     // 0x100 이 할당이 됨.
     this.modelMapper = new ModelMapper();
     // 추가 설정은,
+    this.modelMapper.getConfiguration()
+        // VO <-> DTO 간의 멤버의 일치성 여부를 확인
+        .setFieldMatchingEnabled(true)
+        // 멤버의 접근 지정자를 private 에 접근하기위해서 필요한 설정.
+        // 기본 , public , default
+    .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+        // VO <-> DTO 간의 검사를 , 꼼꼼하게 해주세요.(정확히 일치해야함.)
+        .setMatchingStrategy(MatchingStrategies.STRICT);
+  } // 기본 생성자 닫기. MapperUtil
 
+  // ModelMapper 타입의 인스턴스를 반환하는 메서드 만들기.
+  public ModelMapper get() {
+    return modelMapper;
   }
 }
