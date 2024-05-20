18fb2b9e33b47ba5c771621f0d5a715356c293d6
LeeSangYong <lsy3709@gmail.com>
2024-05-20 17:35:40 +0900

    8교시,1) TodoRegController, 2)TodoService, 화면, 3)todoReg.jsp 입력 로직처리 확인.

--- Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/controller/TodoRegController.java
+++ Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/controller/TodoRegController.java
@@ -1,5 +1,8 @@
 package com.busanit501.samplejsp501.todo.controller;
 
+import com.busanit501.samplejsp501.todo.dto.TodoDTO;
+import com.busanit501.samplejsp501.todo.service.TodoService;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,9 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 
 @WebServlet(name = "todoReg", urlPatterns = "/todo/register")
 public class TodoRegController extends HttpServlet {
+
+  //주입 , 서비스 인스턴스 , 포함.
+  private TodoService todoService = TodoService.INSTANCE;
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     //todo 글 입력 폼
@@ -20,8 +28,21 @@ public class TodoRegController extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     // todo 글 입력 처리 하는 로직.
-    // 리다이렉트, 메인: 리스트
-    resp.sendRedirect("/todo/list");
+    // TodoDTO 타입을 받아서, 서비스에 전달하는 로직.
+    TodoDTO todoDTO = TodoDTO.builder()
+        .title(req.getParameter("title"))
+        .dueDate(LocalDate.parse(req.getParameter("dueDate")))
+        .build();
+
+    // 실제 데이터 입력하기.
+    try {
+      todoService.register2(todoDTO);
+      // 리다이렉트, 메인: 리스트
+      resp.sendRedirect("/todo/list");
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
   }
 }
 
