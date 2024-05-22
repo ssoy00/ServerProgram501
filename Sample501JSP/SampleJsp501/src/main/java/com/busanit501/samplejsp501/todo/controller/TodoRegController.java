3fdf862b897671a4ade9fa36867bd74c7ff39653
LeeSangYong <lsy3709@gmail.com>
2024-05-22 11:26:09 +0900

    3교시, 세션을 이용해서, 게터,세터 활용해서, 로그인 이용해보기1

--- Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/controller/TodoRegController.java
+++ Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/controller/TodoRegController.java
@@ -2,6 +2,7 @@ package com.busanit501.samplejsp501.todo.controller;
 
 import com.busanit501.samplejsp501.todo.dto.TodoDTO;
 import com.busanit501.samplejsp501.todo.service.TodoService;
+import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,9 +10,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 
+@Log4j2
 @WebServlet(name = "todoReg", urlPatterns = "/todo/register")
 public class TodoRegController extends HttpServlet {
 
@@ -20,6 +23,30 @@ public class TodoRegController extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    // 세션의 정보 조회 해보기.
+    HttpSession session = req.getSession();
+
+    // 조회 해보기.
+    // 웹브라우저 -> 서버에 최초로 접근을 하면, 새로운 시스템 쿠키를 만들어주고,
+    // 로그인으로 돌려 보내기.
+    if(session.isNew()){
+      log.info("뉴 JSessionID: 새로운 유저" );
+      resp.sendRedirect("/login");
+      return;
+    }
+
+    // 세션의 로그인 정보를 저장할 공간을 예) loginInfo 라는 공간에
+    // 세션에 로그인 정보가 있다면, 성공 로그인 후 -> 리스트
+    // 로그인 정보가 없다면, 다시, 로그인 폼 이동.
+    if(session.getAttribute("loginInfo") == null){
+      log.info("로그인 정보가 없는 유저 ");
+      resp.sendRedirect("/login");
+      return;
+    }
+
+    // 정상 로그인.
+
     //todo 글 입력 폼
     RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/todo/todoReg.jsp");
     requestDispatcher.forward(req, resp);
