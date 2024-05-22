68fc09670e31a5dc143309f57c340d0ba80a817f
LeeSangYong <lsy3709@gmail.com>
2024-05-22 15:17:46 +0900

    6교시, MemberService 한명의 멤버를 조회하는 서비스 기능테스트 확인

---
+++ Sample501JSP/SampleJsp501/src/test/java/com/busanit501/samplejsp501/service/MemberServiceTests.java
@@ -0,0 +1,35 @@
+package com.busanit501.samplejsp501.service;
+
+import com.busanit501.samplejsp501.todo.dto.MemberDTO;
+import com.busanit501.samplejsp501.todo.service.MemberService;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@Log4j2
+public class MemberServiceTests {
+
+  private MemberService memberService;
+
+  // 아래에 다른 메서드들이 실행되기전에, 먼저 실행이 됨.(역할, todoService 초기화 해주는 역할)
+  @BeforeEach
+  public void ready(){
+    memberService = MemberService.INSTANCE;
+  }
+
+  @Test
+  public  void getOneMemberTest() throws Exception {
+
+    // 인자 값으로 TodoDTO 를 사용해야함.
+    MemberDTO memberDTO = memberService.getOneMember("lsy","1234");
+    log.info("memberDTO : "+ memberDTO);
+  }
+}
+
+
+
+
+
+
+
+
