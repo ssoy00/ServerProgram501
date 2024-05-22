4114b2c6667f81d0cd30414bd581add8c708e14e
LeeSangYong <lsy3709@gmail.com>
2024-05-22 15:40:39 +0900

    6교시, 로그인 후, 리스트에 로그인한 사용자 표시하기. 세션에 MemberDTO 인스턴스 가져와서 이용함

--- Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/controller/TodoListController.java
+++ Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/todo/controller/TodoListController.java
@@ -1,5 +1,6 @@
 package com.busanit501.samplejsp501.todo.controller;
 
+import com.busanit501.samplejsp501.todo.dto.MemberDTO;
 import com.busanit501.samplejsp501.todo.dto.TodoDTO;
 import com.busanit501.samplejsp501.todo.service.TodoService;
 import lombok.extern.log4j.Log4j2;
@@ -9,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -24,9 +26,14 @@ public class TodoListController extends HttpServlet {
    // DB 에서 , 전체 목록을 가져오기.
 
     // 설정한 세션의 정보 가져오기.
+    // 값 문자열 타입.
 //    HttpSession session = req.getSession();
 //    String loginInfoSession = (String) session.getAttribute("loginInfo");
 //    log.info("Login info 세션의 정보 get하기.: " + loginInfoSession);
+    // 방법2 , 값 인스턴스
+    HttpSession session = req.getSession();
+    MemberDTO memberDTO = (MemberDTO) session.getAttribute("loginInfo");
+    log.info("Login info 세션의 정보 get하기.: " + memberDTO);
 
     try {
       //todoService.listAll(); -> 디비에서, 전체 목록 가져오기.
@@ -35,7 +42,7 @@ public class TodoListController extends HttpServlet {
 
       // 컨트롤러에서 (서버)-> 화면(jsp)에 -> 데이터 전달
       req.setAttribute("list",sampleList);
-//      req.setAttribute("mid",loginInfoSession);
+      req.setAttribute("memberDTO",memberDTO);
       req.getRequestDispatcher("/WEB-INF/todo/todoList.jsp")
           .forward(req, resp);
     } catch (Exception e) {
