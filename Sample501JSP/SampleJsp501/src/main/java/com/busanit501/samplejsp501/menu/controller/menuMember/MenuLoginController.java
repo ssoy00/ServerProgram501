615c52cef33dc76b8ca0e0e8b7d8374b740d99ea
LeeSangYong <lsy3709@gmail.com>
2024-05-24 11:21:58 +0900

    3교시, 미니실습 풀이중.로그인 필터, 세션, 쿠키, uuid 구조 설명 기능 확인.

--- Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/menu/controller/menuMember/MenuLoginController.java
+++ Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/menu/controller/menuMember/MenuLoginController.java
@@ -6,11 +6,9 @@ import com.busanit501.samplejsp501.menu.service.MenuMemberService;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet(name = "menuLoginController", urlPatterns = "/menuLogin")
 public class MenuLoginController extends HttpServlet {
@@ -30,17 +28,66 @@ public class MenuLoginController extends HttpServlet {
     String mid = req.getParameter("mid");
     String mpw = req.getParameter("mpw");
 
-    // 디비에서 화면에서 받은 ID, PW  있는지 확인.
+    // 자동 로그인 체크 여부를 값을 가져오기.
+    String auto = req.getParameter("auto");
+    // 상태 변수, 자동 로기은 체크 여부
+    boolean rememberMe = auto != null && auto.equals("on");
+
+    // rememberMe , 체크가 되었다면, UUID를 생성함.
+    // UUID 자동 랜덤한 문자열을 생성해줌. 중복을 피하는 용도로 사용함.
+
     try {
-      MenuMemberDTO menuMemberDTO = MenuMemberService.INSTANCE.getOneMember(mid,mpw);
-      // 자동로그인 체크를 안했을 경우. 로직.
-      HttpSession session = req.getSession();
-      //세션의 정보를 저장.
-      // memberDTO 여기에 어떤 정보가 있나요? mid, mpw, mname, uuid(없음.)
-      session.setAttribute("loginInfo", menuMemberDTO);
-      resp.sendRedirect("/menu/list");
+      // mid, mpw, 해당 유저가 있다면, 회원 정보 가져오기, Todo 상세보기와 같음.
+      // 아직, uuid 가 없음.
+      MenuMemberDTO menuMemberDTO = MenuMemberService.INSTANCE.getOneMember(mid, mpw);
+
+      // 자동로그인 여부가 체크가 되었다면.
+      if (rememberMe) {
+
+        // 디비에 자동로그인 체크 하기.
+//        MemberService.INSTANCE.checkAutoLogin(mid,true);
+
+        // 랜덤한 문자열을 생성.
+        String uuid = UUID.randomUUID().toString();
+        // 랜덤한 문자열을 , 위에서 받아온 정보에, uuid 업데이트하기. memberDTO
+        // 디비에 데이터를 업데이트 하는 부분,
+        MenuMemberService.INSTANCE.updateUUID(mid, uuid);
+        // 임시 모델에, 같은 uuid 담는 코드.
+        menuMemberDTO.setUuid(uuid);
+        // 자동로그인 체크 하기.
+//        memberDTO.setAutoLogined(true);
+
+        //쿠키에 , 생성 uuid 랜덤 문자열 넣기.
+        // 쿠키에 uuid 담기.
+        // 쿠키 생성 기본 인스턴스 만들기. (키 이름 , 값 )
+        Cookie rememberCookie = new Cookie("remember-me",uuid);
+        // 유효기간, 1주일.
+        rememberCookie.setMaxAge(60*60*24*7);
+        // 적용범위. / 하위 전체
+        rememberCookie.setPath("/");
+        // 웹브라우저에게 전달하기(쿠키를)
+        resp.addCookie(rememberCookie);
+
+        // 세션의 설정하기 위한 도구를 가지고 오고,
+        // 세터, 게터
+        HttpSession session = req.getSession();
+        //세션의 정보를 저장.
+        // memberDTO 여기에 어떤 정보가 있나요? mid, mpw, mname, uuid(방금추가됨)
+        session.setAttribute("loginInfo", menuMemberDTO);
+
+
+        resp.sendRedirect("/todo/list");
+      } else {
+        // 자동로그인 체크를 안했을 경우. 로직.
+        HttpSession session = req.getSession();
+        //세션의 정보를 저장.
+        // memberDTO 여기에 어떤 정보가 있나요? mid, mpw, mname, uuid(없음.)
+        session.setAttribute("loginInfo", menuMemberDTO);
+        resp.sendRedirect("/todo/list");
+      }
+
     } catch (Exception e) {
-      resp.sendRedirect("/menuLogin?result=error");
+      resp.sendRedirect("/login?result=error");
     }
 
 
---
+++ Sample501JSP/SampleJsp501/src/main/java/com/busanit501/samplejsp501/menu/filter/MenuLoginFilter.java
@@ -0,0 +1,110 @@
+package com.busanit501.samplejsp501.menu.filter;
+
+import com.busanit501.samplejsp501.todo.dto.MemberDTO;
+import com.busanit501.samplejsp501.todo.service.MemberService;
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+
+@Log4j2
+@WebFilter(urlPatterns = {"/menu/*"})
+public class MenuLoginFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    // 기본 설정,
+    // 동작 여부 확인.
+    log.info("로그인 필터 동작 여부 확인. ");
+
+    // ServletRequest는 HttpServletRequest 의 상위. -> 다운캐스팅.
+
+    HttpServletRequest req = (HttpServletRequest) servletRequest;
+    HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+    // 세션을 이용하기위해서, 도구 필요. 도구 준비.
+    HttpSession session = req.getSession();
+    // 로그인시, 세션의 임시 공간의 이름 : loginInfo ,
+    // 로그인을 해야 -> 세션 생성. -> 필터를 통과해요.
+    //
+    // 세션이 있다? 그러면 로그인 을 했다.
+    // 세션이 없다? 로그인 안했다.
+    if (session.getAttribute("loginInfo") != null) {
+//      resp.sendRedirect("/login");
+      // loginInfo 키가 있다면, 로그인 필터의 doFilter 진행을 계속 하겠다.
+      filterChain.doFilter(servletRequest, servletResponse);
+      return;
+    }
+
+    // session 에 loginInfo 키가 없다면,
+    // 쿠키를 체크.
+    //req.getCookies(), 웹브라우저의 전체 쿠키 목록.
+    // "remember-me" , 찾고자 하는 쿠키 이름(키)
+    Cookie cookie = findCookie(req.getCookies(), "remember-me");
+
+    // 세션에도 없고, 쿠키도 없다면, 그냥 로그인
+    if (cookie == null) {
+      resp.sendRedirect("/login");
+      return;
+    }
+
+    // 쿠키가 존재하는 상황. 쿠키에서 uuid 를 가져오기.
+    String uuid = cookie.getValue();
+
+    try {
+      // uuid 를 이용해서, 한명의 회원을 조회가능.
+      MemberDTO memberDTO = MemberService.INSTANCE.selectUUID(uuid);
+
+      // memberDTO, 회원이 없다면,
+      if(memberDTO == null) {
+        // 강제로 예외 발생시키키
+        throw new Exception("쿠키 값에 해당하는 유저가 없다.");
+      }
+
+      // memberDTO, 회원이 있다면,
+      // 세션에 저장하기.
+      session.setAttribute("loginInfo", memberDTO);
+      //
+      filterChain.doFilter(servletRequest, servletResponse);
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+  } // doFilter
+
+  private Cookie findCookie(Cookie[] cookies, String cookieName) {
+    // 유효성 체크 1
+   if(cookies == null || cookies.length == 0) {
+     return null;
+   }
+   // Optional<Cookie> : 결과의 값이 존재 한다면, 그 값을 사용하고, 없다면 null 반환.
+    // Arrays 시스템 클래스,
+    // stream : 병렬처리, 중간 처리, 최종 처리.
+    // cookies : 전체 쿠키의 목록.
+    // filter : 특정조건에 맞는 요소를 반환하는 기능.
+    // cookie.getName().equals(cookieName)
+    // 모든 쿠키에서 , 하나씩 각각 전부 다꺼내서 다 비교함. 키의 이름: "remember-me"
+    // findFirst() : 찾는 것중에서, 최초에 먼저 발견된 쿠키를 반환함.
+   Optional<Cookie> result = Arrays.stream(cookies)
+       .filter(cookie -> cookie.getName().equals(cookieName)).findFirst();
+    // 유효성 체크를 하려고, 나중에 -> 연산자 이용해서 조금더 간단히 표현할 예정.
+
+return result.isPresent() ? result.get() : null;
+
+  } // findCookie 닫는 부분
+} // LoginFilter
+
+
+
+
+
+
+
