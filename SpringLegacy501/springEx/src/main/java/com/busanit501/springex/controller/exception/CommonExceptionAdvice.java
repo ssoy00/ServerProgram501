471275bbb78894de2c24b942d96fa9a44945c006
LeeSangYong <lsy3709@gmail.com>
2024-05-27 17:40:21 +0900

    8교시, Spring MVC , 예외처리. 1 클래스에 예외처리 포괄적으로 404 not found web.xml 설정 추가.

--- SpringLegacy501/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
+++ SpringLegacy501/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
@@ -1,9 +1,12 @@
 package com.busanit501.springex.controller.exception;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Arrays;
 
@@ -14,17 +17,17 @@ public class CommonExceptionAdvice {
   // REST API , RestController, 화면없이 데이터만 전달시
   // ResponseBody, http 전달시, 헤더와 바디(데이터)
   // 결론, 데이터만 전달 하기.
-  @ResponseBody
-  // 예외가 발생했을 경우 처리할 클래스를 지금은 구체적으로 작성하지만,
-  // 일반적으로, 상위 개념인 Exception으로 처리할 예정.
-  @ExceptionHandler(NumberFormatException.class)
-  public String exceptNumberFormat(NumberFormatException numberFormatException) {
-
-    log.error("----------error----------------");
-    log.error("numberFormatException.getMessage() : " + numberFormatException.getMessage());
-    return "Number Format Exception !!!";
-
-  }
+//  @ResponseBody
+//  // 예외가 발생했을 경우 처리할 클래스를 지금은 구체적으로 작성하지만,
+//  // 일반적으로, 상위 개념인 Exception으로 처리할 예정.
+//  @ExceptionHandler(NumberFormatException.class)
+//  public String exceptNumberFormat(NumberFormatException numberFormatException) {
+//
+//    log.error("----------error----------------");
+//    log.error("numberFormatException.getMessage() : " + numberFormatException.getMessage());
+//    return "Number Format Exception !!!";
+//
+//  }
 
   // 좀더 포괄적으로 예외를 처리 해보자.
   @ResponseBody
@@ -44,6 +47,15 @@ public class CommonExceptionAdvice {
 
     return buffer.toString();
 
+  }
+
+  //예제, 404 Not Found 발생시 , 유저에게 화면 보여주기.
+  @ExceptionHandler(NoHandlerFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String notFound() {
+    // 404 페이지로 전달.
+    // custom404.jsp 만들예정.
+    return "custom404";
   }
 
 }
