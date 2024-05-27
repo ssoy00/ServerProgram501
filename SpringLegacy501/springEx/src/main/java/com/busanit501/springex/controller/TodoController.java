459528a521e77f1eafe661d42d0d80006ca3135b
LeeSangYong <lsy3709@gmail.com>
2024-05-27 11:48:53 +0900

    3교시, Spring MVC , 화면 경로 todo/register.jsp 테스트

--- SpringLegacy501/springEx/src/main/java/com/busanit501/springex/controller/TodoController.java
+++ SpringLegacy501/springEx/src/main/java/com/busanit501/springex/controller/TodoController.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 // 화면상에서 접근하는 URL 주소를 맵핑 해주는 역할.
@@ -15,9 +16,13 @@ public class TodoController {
   public  void listTest() {
     // 최종 경로 : http://localhost:8080/todo/list
     // 최종 경로 : /todo/list
-
     log.info("todo list 조회 화면 테스트 콘솔");
   }
+  @RequestMapping(value = "/register", method = RequestMethod.GET)
+  public void registerTest() {
+    log.info("todo register 등록 화면 테스트 콘솔");
+  }
+
 }
 
 
---
+++ SpringLegacy501/springEx/src/main/webapp/WEB-INF/views/todo/register.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
+<html>
+<head>
+  <title>Title</title>
+</head>
+<body>
+  <h1>컨트롤러 :/todo/register 의 연결 화면: register.jsp 임시 화면. </h1>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
