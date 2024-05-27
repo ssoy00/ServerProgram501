ccda74486e3404fc9dfdfe9a1855ebcebad33d7b
LeeSangYong <lsy3709@gmail.com>
2024-05-27 17:21:22 +0900

    8교시, Spring MVC , 예외처리. 1 클래스에 예외처리 메서드 작성,

--- SpringLegacy501/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
+++ SpringLegacy501/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
@@ -2,11 +2,28 @@ package com.busanit501.springex.controller.exception;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 @Log4j2
 public class CommonExceptionAdvice {
-  
+
+  // REST API , RestController, 화면없이 데이터만 전달시
+  // ResponseBody, http 전달시, 헤더와 바디(데이터)
+  // 결론, 데이터만 전달 하기.
+  @ResponseBody
+  // 예외가 발생했을 경우 처리할 클래스를 지금은 구체적으로 작성하지만,
+  // 일반적으로, 상위 개념인 Exception으로 처리할 예정.
+  @ExceptionHandler(NumberFormatException.class)
+  public String exceptNumberFormat(NumberFormatException numberFormatException) {
+
+    log.error("----------error----------------");
+    log.error("numberFormatException.getMessage() : " + numberFormatException.getMessage());
+    return "숫자 포맷이 아닌 문자열이 넘어와서 에러가 남 테스트.";
+
+  }
+
 }
 
 
