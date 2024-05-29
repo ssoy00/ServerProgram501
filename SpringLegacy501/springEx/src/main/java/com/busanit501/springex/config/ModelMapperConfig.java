34c08301125549687eed1484aafe7714d5abb990
LeeSangYong <lsy3709@gmail.com>
2024-05-29 10:14:19 +0900

    2교시, config 모델 맵퍼 설정및 root-context.xml 경로 등록.

---
+++ SpringLegacy501/springEx/src/main/java/com/busanit501/springex/config/ModelMapperConfig.java
@@ -0,0 +1,27 @@
+package com.busanit501.springex.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+  @Bean
+  public ModelMapper getMapper() {
+    ModelMapper mapper = new ModelMapper();
+    mapper.getConfiguration()
+        .setFieldMatchingEnabled(true)
+        .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+        .setMatchingStrategy(MatchingStrategies.STRICT);
+    return mapper;
+  }
+}
+
+
+
+
+
+
+
