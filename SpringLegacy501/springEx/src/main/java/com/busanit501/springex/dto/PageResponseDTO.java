f225729e543477df5f707030a538286397fed356
LeeSangYong <lsy3709@gmail.com>
2024-05-30 17:26:21 +0900

    8교시 페이징 정보 응답시 탑재한 정보들 내용 확인

--- SpringLegacy501/springEx/src/main/java/com/busanit501/springex/dto/PageResponseDTO.java
+++ SpringLegacy501/springEx/src/main/java/com/busanit501/springex/dto/PageResponseDTO.java
@@ -1,5 +1,7 @@
 package com.busanit501.springex.dto;
 
+import lombok.Builder;
+
 import java.util.List;
 
 // <E>, 포괄적인 표현
@@ -25,6 +27,23 @@ public class PageResponseDTO<E> {
 
   // 전달할 페이징이 된 todo 10개 목록.
   private List<E> dtoList;
+
+  //생성자 만들기.
+  // builderMethodName = "withAll" -> 기존 빌더 패턴 사용시, TodoVO.builder.build()
+  // builder 의 이름이 -> withAll 교체
+  // pageRequestDTO : 화면 -> 서버 : 페이징 정보, page, size, getSkip,
+  // List<E> dtoList : 10개씩 조회한 Todo 정보들
+  // total : Todo의 전체 갯수
+  // 결론, PageResponseDTO 에, 파라미터의 정보들이 다 들어있다.
+  @Builder(builderMethodName = "withAll")
+  public PageResponseDTO(PageRequestDTO pageRequestDTO, List<E> dtoList, int total){
+    this.page = pageRequestDTO.getPage();
+    this.size = pageRequestDTO.getSize();
+
+    this.total = total;
+    this.dtoList = dtoList;
+  }
+
 }
 
 
