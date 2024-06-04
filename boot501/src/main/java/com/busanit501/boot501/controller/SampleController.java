d4dab6b7211da61a4b191763bac36c46d6a20dbb
LeeSangYong <lsy3709@gmail.com>
2024-06-04 15:39:37 +0900

    6교시 타임리프 기본 문법 확인3, 옵션, 서버에서 넘어온 데이터 html 에서 사용, 자바스크립트 태그안에서 사용

--- boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.busanit501.boot501.controller;
 
+import com.busanit501.boot501.dto.SampleDTO;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 @Log4j2
@@ -36,6 +41,32 @@ public class SampleController {
     log.info("/ex/ex1~~~~~~~~~~~");
     List<String> list = Arrays.asList("도시락","라면","김밥","볶음밥");
     model.addAttribute("list",list);
+  }
+
+  @GetMapping("/ex/ex2")
+  public void ex2(Model model) {
+    log.info("/ex/ex2~~~~~~~~~~~");
+    List<String> list = Arrays.asList("도시락","라면","김밥","볶음밥");
+
+    List<String> list2 = IntStream.range(1,10).mapToObj(i-> "이상용"+i)
+            .collect(Collectors.toList());
+
+    model.addAttribute("list",list);
+    model.addAttribute("list2",list2);
+
+    Map<String,String> map = new HashMap<>();
+    map.put("menu1","도시락");
+    map.put("menu2","된장찌개");
+
+    model.addAttribute("map",map);
+
+    SampleDTO sampleDTO = SampleDTO.builder()
+        .name("이상용")
+        .age("30")
+        .build();
+
+    model.addAttribute("dto",sampleDTO);
+
   }
 
 
---
+++ boot501/src/main/java/com/busanit501/boot501/dto/SampleDTO.java
@@ -0,0 +1,19 @@
+package com.busanit501.boot501.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SampleDTO {
+  private String name;
+  private String age;
+
+}
+
+
+
+
+
+
+
