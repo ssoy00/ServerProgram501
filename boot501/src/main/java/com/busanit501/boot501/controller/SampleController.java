26cd87c79294bdd615a06644cd486e3afa9c2c7c
LeeSangYong <lsy3709@gmail.com>
2024-06-04 10:48:09 +0900

    2교시 테스트 환경에서 롬복 사용하기 설정 추가. build.gradle 추가

---
+++ boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
@@ -0,0 +1,16 @@
+package com.busanit501.boot501.controller;
+
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@Log4j2
+public class SampleController {
+}
+
+
+
+
+
+
+
---
+++ boot501/src/test/java/com/busanit501/boot501/DataSourceTests.java
@@ -0,0 +1,21 @@
+package com.busanit501.boot501;
+
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+public class DataSourceTests {
+
+
+
+}
+
+
+
+
+
+
+
