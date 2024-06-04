00634d531de30fac9d59d3048e141309e0eb1dab
LeeSangYong <lsy3709@gmail.com>
2024-06-04 14:04:26 +0900

    5교시 타임리프 기본 문법 확인1

--- boot501/src/main/java/com/busanit501/boot501/controller/RestSampleController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/RestSampleController.java
@@ -12,6 +12,12 @@ public class RestSampleController {
   public String[] hi(){
     log.info("hi test~~~~rest ");
     return new String[]{"오늘 점심 뭐먹지?","도시락","김치볶음밥"};
+    // 스프링 부트는 기본적으로 잭슨 이라는 이름의 라이브러리 도구가 설치됨.
+    // 역할 : 문자열 -> JSON 타입으로 자동 변환 해주는 도구.
+
+    // 공공 데이터 , 관광관련된 데이터를 오픈소스, 받는 타입을 JSON
+    // JSON -> 원하는 모델에 바로 맵핑.
+
   }
 }
 
--- boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 @Log4j2
 // 데이터만 전달
@@ -26,6 +29,16 @@ public class SampleController {
     log.info("hello2~~~~~~~~~~~");
     model.addAttribute("msg","hello 안녕하세요2");
   }
+
+  //타임리프 연습해보기.
+  @GetMapping("/ex/ex1")
+  public void ex1(Model model) {
+    log.info("/ex/ex1~~~~~~~~~~~");
+    List<String> list = Arrays.asList("도시락","라면","김밥","볶음밥");
+    model.addAttribute("list",list);
+  }
+
+
 }
 
 
