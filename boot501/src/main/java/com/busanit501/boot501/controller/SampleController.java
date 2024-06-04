5457c0ca18455f2a1bc331ac75e683981c1dcd33
LeeSangYong <lsy3709@gmail.com>
2024-06-04 12:15:51 +0900

    4교시 기본 틀, 컨트롤러, 기본 화면 구성 확인 타임리프

--- boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
@@ -2,10 +2,20 @@ package com.busanit501.boot501.controller;
 
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@Data
+
 @Log4j2
+@Controller
 public class SampleController {
+
+  @GetMapping("/hello")
+  public void hello(Model model) {
+    log.info("hello~~~~~~~~~~~");
+    model.addAttribute("msg","hello 안녕하세요");
+  }
 }
 
 
