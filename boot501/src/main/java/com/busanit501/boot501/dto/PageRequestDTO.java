2ec931d93d0be84bcc5f61d800e9b46c92017739
이상용 <lsy3709@naver.com>
2024-06-07 10:25:24 +0900

    2교시 페이징 준비물 준비 작업 PageRequestDTO 만들기 2 타입 분리, 페이징 정보 반환

--- boot501/src/main/java/com/busanit501/boot501/dto/PageRequestDTO.java
+++ boot501/src/main/java/com/busanit501/boot501/dto/PageRequestDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @Builder
 @Data
@@ -22,4 +25,24 @@ public class PageRequestDTO {
 
     private String keyword;
 
+    // 타입 분리작업
+    public String[] getTypes() {
+        if(type == null || type.isEmpty()) {
+            return null;
+        }
+        //예시 type={"twc"} -> "t", "w", "c" , 분리함.
+        return type.split("");
+    }
+
+    // 페이징 정보 반환하기.
+    // ...props , 가변 인자 -> (String title, String content, ...)
+    // 매개변수에 인자 값으로 여러개의 문자열 타입의 요소를 추가 가능.
+    public Pageable getPageable(String...props) {
+        // 화면에서 1페이지 -> 0
+        // 화면에서 2페이지 -> 1
+        return PageRequest.of(this.page - 1,10, Sort.by(props).descending());
+    }
+
 }
+
+
