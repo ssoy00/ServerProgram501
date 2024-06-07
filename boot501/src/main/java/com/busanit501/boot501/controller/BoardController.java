d1f6a2740a33df52b9ad49950c0ca52d0aba654a
이상용 <lsy3709@naver.com>
2024-06-07 15:16:33 +0900

    6교시 등록 작업, 서버에서 글쓰기 작업 하기 BoardController

--- boot501/src/main/java/com/busanit501/boot501/controller/BoardController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/BoardController.java
@@ -4,13 +4,17 @@ import com.busanit501.boot501.dto.BoardDTO;
 import com.busanit501.boot501.dto.PageRequestDTO;
 import com.busanit501.boot501.dto.PageResponseDTO;
 import com.busanit501.boot501.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 // 화면, 데이터 같이 전달.
 @Controller
@@ -28,8 +32,31 @@ public class BoardController {
         // 서버로부터 응답확인.
         log.info("BoardController 확인 중, responseDTO : " + responseDTO);
         // 서버 -> 화면 데이터 전달.
-        model.addAttribute("responseDTO",responseDTO);
+        model.addAttribute("responseDTO", responseDTO);
 
     } //list 닫는 부분
+
+    //글쓰기
+    @PostMapping("/register")
+    public String register(@Valid BoardDTO boardDTO
+            , BindingResult bindingResult
+            , RedirectAttributes redirectAttributes
+            , Model model) {
+        // 입력중 유효성 체크에 해당 될 때
+        if(bindingResult.hasErrors()) {
+            log.info("register 중 오류 발생.");
+            redirectAttributes.addFlashAttribute(
+                    "errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+        log.info("화면에서 입력 받은 내용 확인 : " + boardDTO);
+
+        //화면 -> 서버 -> 서비스 -> 레포지토리 -> 디비, 입력후, 게시글 번호 가져오기
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result",bno);
+        return "redirect:/board/list";
+
+    }
 
 } // BoardController 닫는 부분
