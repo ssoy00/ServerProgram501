2e0c6519e4ee0b6a1905c2eded0821b9d8689fce
이상용 <lsy3709@naver.com>
2024-06-07 15:09:06 +0900

    6교시 등록 작업, 서버에서 유효성 검사 도구 build.gradle 설치, BoardDTO 작업

--- boot501/src/main/java/com/busanit501/boot501/dto/BoardDTO.java
+++ boot501/src/main/java/com/busanit501/boot501/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package com.busanit501.boot501.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,8 +17,15 @@ import java.time.LocalDateTime;
 //
 public class BoardDTO {
   private Long bno;
+
+  @NotEmpty
+  @Size(min = 3, max = 100)
   private String title;
+
+  @NotEmpty
   private String content;
+
+  @NotEmpty
   private String writer;
   private LocalDateTime regDate;
   private LocalDateTime modDate;
