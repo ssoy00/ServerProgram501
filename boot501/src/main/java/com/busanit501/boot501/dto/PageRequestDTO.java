33bee8d3820a2d425b49dbc8eb341d0326ac6c3b
이상용 <lsy3709@naver.com>
2024-06-07 10:32:53 +0900

    2교시 페이징 준비물 준비 작업 PageRequestDTO 만들기 3 getLink 에 기본 페이징, 검색 조건 넣기

--- boot501/src/main/java/com/busanit501/boot501/dto/PageRequestDTO.java
+++ boot501/src/main/java/com/busanit501/boot501/dto/PageRequestDTO.java
@@ -8,6 +8,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 @Builder
 @Data
 @AllArgsConstructor
@@ -25,9 +28,13 @@ public class PageRequestDTO {
 
     private String keyword;
 
+    // 페이징 정보와, 검색 정보를 , URL에서, 쿼리스트링으로
+    // 파라미터를 한번에 작성 하기위한 멤버.
+    private String link;
+
     // 타입 분리작업
     public String[] getTypes() {
-        if(type == null || type.isEmpty()) {
+        if (type == null || type.isEmpty()) {
             return null;
         }
         //예시 type={"twc"} -> "t", "w", "c" , 분리함.
@@ -37,11 +44,38 @@ public class PageRequestDTO {
     // 페이징 정보 반환하기.
     // ...props , 가변 인자 -> (String title, String content, ...)
     // 매개변수에 인자 값으로 여러개의 문자열 타입의 요소를 추가 가능.
-    public Pageable getPageable(String...props) {
+    public Pageable getPageable(String... props) {
         // 화면에서 1페이지 -> 0
         // 화면에서 2페이지 -> 1
-        return PageRequest.of(this.page - 1,10, Sort.by(props).descending());
+        return PageRequest.of(this.page - 1, 10, Sort.by(props).descending());
     }
+
+    // 페이징 정보와, 검색 정보를 , URL에서, 쿼리스트링으로
+    // 파라미터를 한번에 작성 하기위한 멤버.
+    public String getLink() {
+        if (link == null) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("page=" + this.page);
+            stringBuilder.append("&size=" + this.size);
+
+            // 타입, 검색 조건, t,w,c
+            if (type != null && type.length() > 0) {
+                stringBuilder.append("&type=" + type);
+            }
+
+            // 검색어, keyword
+            if (keyword != null) {
+                try {
+                    stringBuilder.append("&keyword=" + URLEncoder.encode(keyword, "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            link = stringBuilder.toString();
+
+        } // if null 닫는 태그
+        return link;
+    } // getLink 닫는 태그
 
 }
 
