a0f4cc0c0f3e5361ae0971a810ad7606fa91dd4c
이상용 <lsy3709@naver.com>
2024-06-07 11:42:47 +0900

    3교시 화면 베이스 레이아웃 수정중

---
+++ boot501/src/main/java/com/busanit501/boot501/controller/BoardController.java
@@ -0,0 +1,24 @@
+package com.busanit501.boot501.controller;
+
+import com.busanit501.boot501.dto.PageRequestDTO;
+import com.busanit501.boot501.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 화면, 데이터 같이 전달.
+@Controller
+@RequestMapping("/board")
+@Log4j2
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    // ex) /board/list
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {}
+}
