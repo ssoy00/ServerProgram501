88591e301b998d0540c697efe7a2de7c2be6ea3c
이상용 <lsy3709@naver.com>
2024-06-11 12:27:54 +0900

    4교시, Swagger UI 설치4, 설정 클래스 추가2

---
+++ boot501/src/main/java/com/busanit501/boot501/config/CustomServletConfig.java
@@ -0,0 +1,9 @@
+package com.busanit501.boot501.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class CustomServletConfig {
+}
