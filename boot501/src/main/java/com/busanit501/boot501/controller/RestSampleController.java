4dd6b0e3cd8b36983e154368cdc484aab469ab7a
이상용 <lsy3709@naver.com>
2024-06-11 12:24:52 +0900

    4교시, Swagger UI 설치3, 레스트 클래스에 태그 달아서 적용하기

--- boot501/src/main/java/com/busanit501/boot501/controller/RestSampleController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/RestSampleController.java
@@ -1,17 +1,22 @@
 package com.busanit501.boot501.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 데이터만 전달하기.
 @RestController
 @Log4j2
 public class RestSampleController {
-  @GetMapping("/hiRest")
+
+  @Tag(name = "샘플 테스트", description = "post 샘플 테스트입니다.")
+  @PostMapping("/hiRest")
   public String[] hi(){
     log.info("hi test~~~~rest ");
-    return new String[]{"오늘 점심 뭐먹지?","도시락","김치볶음밥"};
+    return new String[]{"오늘 점심 뭐먹지?","제육볶음","돌솥비빔밥"};
     // 스프링 부트는 기본적으로 잭슨 이라는 이름의 라이브러리 도구가 설치됨.
     // 역할 : 문자열 -> JSON 타입으로 자동 변환 해주는 도구.
 
