4a0d3ac32b6493793d784913e99bc8579df98373
이상용 <lsy3709@naver.com>
2024-06-12 12:33:53 +0900

    4교시, Repository 에서 BoardSearch에서, 댓글 갯수를 포함한 목록 메서드 추가

--- boot501/src/main/java/com/busanit501/boot501/repository/search/BoardSearch.java
+++ boot501/src/main/java/com/busanit501/boot501/repository/search/BoardSearch.java
@@ -1,6 +1,7 @@
 package com.busanit501.boot501.repository.search;
 
 import com.busanit501.boot501.domain.Board;
+import com.busanit501.boot501.dto.BoardListReplyCountDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -8,6 +9,11 @@ public interface BoardSearch {
   Page<Board> search(Pageable pageable);
 
   Page<Board> searchAll(String[] types, String keyword ,Pageable pageable);
+
+  // 댓글 개수를 포함한 전체 게시글 목록.
+  Page<BoardListReplyCountDTO> searchWithReplyCount(
+          String[] types, String keyword ,Pageable pageable
+  );
 
 }
 
