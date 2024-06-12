7794f3d2636d28f8d3c777c607f6f21d5aa6a198
이상용 <lsy3709@naver.com>
2024-06-12 11:47:07 +0900

    3교시, 보드의 게시글 번호에 댓글 작성 단위테스트 확인

--- boot501/src/test/java/com/busanit501/boot501/repository/ReplyRepositoryTest.java
+++ boot501/src/test/java/com/busanit501/boot501/repository/ReplyRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.busanit501.boot501.repository;
 
+import com.busanit501.boot501.domain.Board;
+import com.busanit501.boot501.domain.Reply;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +11,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Log4j2
 public class ReplyRepositoryTest {
     @Autowired
-    private ReplyRepositoryTest replyRepository;
+    private ReplyRepository replyRepository;
 
     @Test
     public void testInsert() {
+        //실제 디비 각자 데이터에 따라서 다름.
+        // 현재 bno = 900
+        Long bno = 900L;
+
+        Board board = Board.builder()
+                .bno(bno)
+                .build();
+
+        Reply reply = Reply.builder()
+                .board(board)
+                .replyText("오늘 점심은 면으로")
+                .replyer("이상용")
+                .build();
+
+        replyRepository.save(reply);
 
     }
 
