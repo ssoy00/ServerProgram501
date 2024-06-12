07adb609b4e2e0a5bfe1d9c52d8f4113bb45821f
이상용 <lsy3709@naver.com>
2024-06-12 14:27:25 +0900

    5교시, 서비스 , 댓글 갯수 보여주기 작업

--- boot501/src/main/java/com/busanit501/boot501/service/BoardService.java
+++ boot501/src/main/java/com/busanit501/boot501/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.busanit501.boot501.service;
 
 import com.busanit501.boot501.dto.BoardDTO;
+import com.busanit501.boot501.dto.BoardListReplyCountDTO;
 import com.busanit501.boot501.dto.PageRequestDTO;
 import com.busanit501.boot501.dto.PageResponseDTO;
 
@@ -14,6 +15,9 @@ public interface BoardService {
   // 화면에서, 사용자가, 현재 페이지 12, 사이지 : 10개 씩 보고
   // 검색어가 존재하고, 타입도 있고,
   PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO);
+  //댓글 숫자 포함해서 목록 출력하기.
+  PageResponseDTO<BoardListReplyCountDTO> listWithReplyCount(PageRequestDTO pageRequestDTO);
+
 }
 
 
--- boot501/src/main/java/com/busanit501/boot501/service/BoardServiceImpl.java
+++ boot501/src/main/java/com/busanit501/boot501/service/BoardServiceImpl.java
@@ -2,6 +2,7 @@ package com.busanit501.boot501.service;
 
 import com.busanit501.boot501.domain.Board;
 import com.busanit501.boot501.dto.BoardDTO;
+import com.busanit501.boot501.dto.BoardListReplyCountDTO;
 import com.busanit501.boot501.dto.PageRequestDTO;
 import com.busanit501.boot501.dto.PageResponseDTO;
 import com.busanit501.boot501.repository.BoardRepository;
@@ -85,8 +86,31 @@ public class BoardServiceImpl implements BoardService {
             .dtoList(dtoList)
             .total((int) result.getTotalElements())
             .build();
+    //test
 
     return pageResponseDTO;
+  }
+
+  @Override
+  public PageResponseDTO<BoardListReplyCountDTO> listWithReplyCount
+          (PageRequestDTO pageRequestDTO) {
+    String[] types = pageRequestDTO.getTypes();
+    String keyword = pageRequestDTO.getKeyword();
+    Pageable pageable = pageRequestDTO.getPageable("bno");
+
+    // 댓글 갯수를 포함한 목록을 가져오기.
+    Page<BoardListReplyCountDTO> result = boardRepository.searchWithReplyCount(types,keyword,pageable);
+    // BoardListReplyCountDTO 변환 할 필요가 일단 없다.
+
+    // 서버 -> 화면에 전달할 준비물 준비 작업 완료.
+    // 1)페이지 2) 사이즈 3) 전쳇갯수 4) 검색 결과 내역10개(엔티티-> DTO)
+    PageResponseDTO pageResponseDTO = PageResponseDTO.<BoardListReplyCountDTO>withAll()
+            .pageRequestDTO(pageRequestDTO)
+            .dtoList(result.getContent())
+            .total((int) result.getTotalElements())
+            .build();
+
+    return null;
   }
 }
 
