7d17402600a778196e05b8920994d5ff901b1a3d
이상용 <lsy3709@naver.com>
2024-06-12 09:50:13 +0900

    0612 1교시,댓글 쓰기 단위테스트 준비중

---
+++ boot501/src/main/java/com/busanit501/boot501/repository/ReplyRepository.java
@@ -0,0 +1,7 @@
+package com.busanit501.boot501.repository;
+
+import com.busanit501.boot501.domain.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+}
---
+++ boot501/src/test/java/com/busanit501/boot501/repository/ReplyRepository.java
@@ -0,0 +1,19 @@
+package com.busanit501.boot501.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class ReplyRepository {
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    public void testInsert() {
+
+    }
+
+}
