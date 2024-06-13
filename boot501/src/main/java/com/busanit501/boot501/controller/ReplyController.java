e44e2226fdf64ddd59a16d5d3170842217a4d10d
이상용 <lsy3709@naver.com>
2024-06-13 10:27:46 +0900

    2교시, 댓글 목록 컨트롤러 확인, 댓글 수정

--- boot501/src/main/java/com/busanit501/boot501/controller/ReplyController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/ReplyController.java
@@ -89,4 +89,34 @@ public class ReplyController {
     } //getList
 
 
+    @Tag(name = "특정 댓글 수정 put 방식", description = "특정 댓글 수정 put 방식")
+    @PostMapping(value = "/{rno}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    //ResponseEntity, 데이터 + http 상태코드 같이 전달 도구.
+//    public ResponseEntity<Map<String,Long>> register(
+    public Map<String,Long> update(
+            //@RequestBody -> JSON 문자열.  , 키: 값의 구조 형태의 중간 데이터 타입,
+            // JSON 문자열 <--Jackson 컨버터 --> DTO
+            @PathVariable("rno") Long rno,
+            @Valid @RequestBody ReplyDTO replyDTO,
+            BindingResult bindingResult) throws BindException {
+        log.info("ReplyController의 update ,replyDTO 확인: "+replyDTO);
+
+        // 에러가 발생한다면, 처리하기.
+        if (bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
+
+        // 응답 해줄 더미 데이터 만들기.
+//        Map<String,Long> resultMap = Map.of("rno",123L);
+        // 실제 데이터를 입력 후, 해당 데이터를 반환
+        Map<String,Long> resultMap = new HashMap<>();
+        replyService.update(replyDTO);
+
+        resultMap.put("rno",rno);
+
+        //        return ResponseEntity.ok(resultMap);
+        return resultMap;
+
+    } // register 닫는 부분
+
 }
