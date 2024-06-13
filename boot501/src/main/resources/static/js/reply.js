cd62a8980e16aab4e4cff2b76ac6ab5144b4961a
이상용 <lsy3709@naver.com>
2024-06-13 11:32:53 +0900

    3교시, reply.js axios 이용해서 비동기 통신 테스트2 , read.html 자바스크립 영역에서 테스트 해보기3

--- boot501/src/main/resources/static/js/reply.js
+++ boot501/src/main/resources/static/js/reply.js
@@ -1,5 +1,7 @@
 async function getTest(bno) {
     const result = await axios.get(`/replies/list/${bno}`)
     // console.log("result 확인 : " + result)
-    return result.data
+    // 데이터 확인용
+    // return result.data
+    return result
 }
