33413bc417e80b8ddf1f72335b05cdb4df8148b2
이상용 <lsy3709@naver.com>
2024-06-13 11:39:29 +0900

    3교시, reply.js axios 이용해서 댓글 목록 출력하는 함수1

--- boot501/src/main/resources/static/js/reply.js
+++ boot501/src/main/resources/static/js/reply.js
@@ -1,3 +1,4 @@
+//axios 통신 테스트용 함수
 async function getTest(bno) {
     const result = await axios.get(`/replies/list/${bno}`)
     // console.log("result 확인 : " + result)
@@ -5,3 +6,16 @@ async function getTest(bno) {
     // return result.data
     return result
 }
+//댓글 목록 출력하는 함수.
+// bno: 현재 게시글 번호
+// page: 페이지번호
+// size: 페이지당 출력 갯수
+// goLast: 마지막에 최신 댓글이 있다면, 마지막 댓글로 가는 여부.
+async function getList({bno,page,size,goLast}){
+   const result = await axios.get(`/replies/list/${bno}`), {params : {page,size}}
+    return result.data
+}
+
+
+
+
