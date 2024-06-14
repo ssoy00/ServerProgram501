26a04f33c2b656c8e4e6a765ed84e41c9d0dfaed
이상용 <lsy3709@naver.com>
2024-06-14 14:46:10 +0900

    5교시, 파일 업로드 테스트 스웨거에서 파일 업로드 하는 부분 체크3, postman 으로 변경, 키:files 변경확인

--- boot501/src/main/java/com/busanit501/boot501/controller/UpDownController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/UpDownController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +18,13 @@ public class UpDownController {
 
     @Tag(name = "파일 업로드 post 방식", description = "파일 업로드 post 방식")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public List<UploadFileDTO> upload(
-//            @Parameter(
-//                    description = "Files to be uploaded",
-//                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)
-//            )
-            UploadFileDTO uploadFileDTO){
+    public String upload(UploadFileDTO uploadFileDTO){
         log.info("UpDownController에서 uploadFileDTO : "+uploadFileDTO);
+        if(uploadFileDTO.getFiles() != null) {
+            uploadFileDTO.getFiles().forEach(multipartFile -> {
+                log.info("파일명 확인: "+multipartFile.getOriginalFilename());
+            });
+        }
         return null;
     }
 }
