642bb559c5d3477e9183905716318c0d799760cd
이상용 <lsy3709@naver.com>
2024-06-14 15:12:01 +0900

    6교시, 파일 업로드 실제 물리 경로에 파일 업로드 하기.

--- boot501/src/main/java/com/busanit501/boot501/controller/UpDownController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/UpDownController.java
@@ -10,7 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @Log4j2
@@ -24,7 +28,22 @@ public class UpDownController {
         log.info("UpDownController에서 uploadFileDTO : "+uploadFileDTO);
         if(uploadFileDTO.getFiles() != null) {
             uploadFileDTO.getFiles().forEach(multipartFile -> {
+                //multipartFile 객체 안에 이미지 파일들이 들어가 있다.
                 log.info("파일명 확인: "+multipartFile.getOriginalFilename());
+                //원본 파일명
+                String originName = multipartFile.getOriginalFilename();
+                // 랜덤한 16자리 문자열
+                String uuid = UUID.randomUUID().toString();
+                // 업로드 경로에 파일 객체를 만들기.
+                Path savePath = Paths.get(uploadPath,uuid+"_"+originName);
+
+                try{
+                    //실제 파일에 저장.
+                    multipartFile.transferTo(savePath);
+                } catch (IOException e) {
+                   e.printStackTrace();
+                }
+
             });
         }
         return null;
