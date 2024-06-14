7c331fdfc1c5e52abc79d6557444e5ce83c1c645
이상용 <lsy3709@naver.com>
2024-06-14 12:45:48 +0900

    4교시, 파일 업로드 레스트 컨트롤로 이용하고, 스웨거 이용해서 테스트 준비, tag 및, 데이터 타입 설정 누락 수정

--- boot501/src/main/java/com/busanit501/boot501/controller/UpDownController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/UpDownController.java
@@ -1,12 +1,18 @@
 package com.busanit501.boot501.controller;
 
 import com.busanit501.boot501.dto.upload.UploadFileDTO;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
 public class UpDownController {
+
+    @Tag(name = "파일 업로드 post 방식", description = "파일 업로드 post 방식")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String upload(UploadFileDTO uploadFileDTO) {
         log.info("UpDownController에서 uploadFileDTO : "+uploadFileDTO);
         return null;
