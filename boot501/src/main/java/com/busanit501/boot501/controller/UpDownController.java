110f2fd39c2c194a737fba3803b0e8c97d2201a7
이상용 <lsy3709@naver.com>
2024-06-14 14:02:54 +0900

    5교시, 파일 업로드 테스트 스웨거에서 파일 업로드 하는 부분 체크

--- boot501/src/main/java/com/busanit501/boot501/controller/UpDownController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/UpDownController.java
@@ -1,11 +1,15 @@
 package com.busanit501.boot501.controller;
 
 import com.busanit501.boot501.dto.upload.UploadFileDTO;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Log4j2
@@ -13,7 +17,12 @@ public class UpDownController {
 
     @Tag(name = "파일 업로드 post 방식", description = "파일 업로드 post 방식")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String upload(UploadFileDTO uploadFileDTO) {
+    public List<UploadFileDTO> upload(
+            @Parameter(
+                    description = "Files to be uploaded",
+                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)
+            )
+            UploadFileDTO uploadFileDTO){
         log.info("UpDownController에서 uploadFileDTO : "+uploadFileDTO);
         return null;
     }
