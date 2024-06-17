bc07891d85bc761a8559c8dc53c984548ca7d296
이상용 <lsy3709@naver.com>
2024-06-17 09:35:31 +0900

    1교시, BoardImage 엔티티 클래스 만들기2

--- boot501/src/main/java/com/busanit501/boot501/domain/BoardImage.java
+++ boot501/src/main/java/com/busanit501/boot501/domain/BoardImage.java
@@ -19,6 +19,10 @@ public class BoardImage implements Comparable<BoardImage> {
 
     private int ord;
 
+    // 1번 게시글 부모 테이블, 0x100
+    // 이미지들이 자식 테이블, 이미지들 예를 3개.
+    // 게시글 삭제를 하면, 자식 테이블은 참조하는 Board 객체가 없음.
+    // 오류가 남. 널포인트 예외, 삭제시, 이미지들의 부모 참조형을 변경하기. null
     @ManyToOne
     private Board board;
 
@@ -29,4 +33,11 @@ public class BoardImage implements Comparable<BoardImage> {
         // 양수는 , 오름차순, 음수 내림차순.
         return this.ord - other.ord;
     }
+
+    // 영속성을 이용해서, 부모 객체를 참고 있다가 , 만약, 부모 객체가 없어진다면,
+    // 고아 객체가 되어서, 자동 삭제 이용할 예정.
+    public void changeBoard(Board board) {
+        this.board = board;
+    }
+
 }
