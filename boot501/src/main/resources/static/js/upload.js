f59cd12cba643df1f8f10adb3ff43133880d31e9
이상용 <lsy3709@naver.com>
2024-06-17 16:05:57 +0900

    7교시, upload.js 이미지 파일 업로드 및 삭제 기능 추가

---
+++ boot501/src/main/resources/static/js/upload.js
@@ -0,0 +1,19 @@
+async function uploadToServer (formObj) {
+    console.log("formObj" + formObj)
+
+    const response = await axios({
+        method: 'post',
+        url : '/upload',
+        data : formObj,
+        headers : {
+            // 오타 주의 , 이름과 값이 정해짐.
+            'Content-Type' : 'multipart/form-data',
+        }
+    });
+    return response.data
+}
+
+async function removeFileToServer(uuid,fileName) {
+    const response = await axios.delete(`/delete/${uuid}_${fileName}`)
+    return response.data
+}
