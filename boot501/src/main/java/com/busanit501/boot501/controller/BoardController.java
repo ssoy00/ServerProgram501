72ea1da34f083791176642fb747262555b0c27d9
이상용 <lsy3709@naver.com>
2024-06-18 12:34:24 +0900

    4교시, 수정폼에서, 삭제하기 서버의 컨트롤러에서, 기존 삭제 메서드 변경, 확인1

--- boot501/src/main/java/com/busanit501/boot501/controller/BoardController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/BoardController.java
@@ -125,7 +125,7 @@ public class BoardController {
 
         //화면 -> 서버 -> 서비스 -> 레포지토리 -> 디비, 입력후, 게시글 번호 가져오기
         //화면 <- 서버 <- 서비스 <- 레포지토리 <- 디비
-        boardService.delete(bno);
+        boardService.deleteAll(bno);
 
         // 글쓰기 후, 작성된 게시글 번호 -> 화면 , 임시로 전달.(1회용)
         redirectAttributes.addFlashAttribute("result",bno);
