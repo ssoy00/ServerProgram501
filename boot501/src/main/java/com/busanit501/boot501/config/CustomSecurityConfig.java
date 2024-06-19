e6f86da42b777ddac01d7ae29334419d7121a03d
이상용 <lsy3709@naver.com>
2024-06-19 10:07:30 +0900

    1교시 정적 자원 시큐리피 필터 항목에서 제외

--- boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
+++ boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
@@ -2,9 +2,11 @@ package com.busanit501.boot501.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
@@ -18,6 +20,15 @@ public class CustomSecurityConfig {
         // 로그인 없이 자동 로그인 확인
         // 빈 설정.
         return http.build();
+    }
+
+    //정적 자원 시큐리티 필터 항목에 제외하기.
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("시큐리티 동작 확인 ====webSecurityCustomizer======================");
+        return (web) ->
+                web.ignoring()
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
 }
