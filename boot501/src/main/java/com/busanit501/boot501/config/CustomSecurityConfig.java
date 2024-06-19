551e59c9afbc01e48e50e4d1cca630eff964f005
이상용 <lsy3709@naver.com>
2024-06-19 09:46:54 +0900

    1교시 설정 클래스 통한 설정 해보기. 로그인 없이 이용해보기

--- boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
+++ boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
@@ -2,10 +2,21 @@ package com.busanit501.boot501.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
 @Configuration
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.info("시큐리티 동작 확인 ====CustomSecurityConfig======================");
+        // 로그인 없이 자동 로그인 확인
+        return http.build();
+    }
+
 }
