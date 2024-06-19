e70dab1a1d99fd19a225064120c085e4e588ac54
이상용 <lsy3709@naver.com>
2024-06-19 10:40:52 +0900

    2교시 시큐리티에서 UserDetails 타입의 유저명, 인코딩된 패스워드 설정 더미 데이터

--- boot501/src/main/java/com/busanit501/boot501/security/CustomUserDetailsService.java
+++ boot501/src/main/java/com/busanit501/boot501/security/CustomUserDetailsService.java
@@ -2,19 +2,40 @@ package com.busanit501.boot501.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    // 스프링 시큐리티 설정 클래스에 빈으로 등록한 인스턴스 주입하기.
+    private PasswordEncoder passwordEncoder;
+
+    //생성자로 주입하기.
+    public CustomUserDetailsService() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("시큐리티 loadUserByUsername 확인 : "+ username);
-        return null;
+
+        //User, 스프링 시큐리티에서 제공하는 기본 클래스,
+        // 사용자 가 만든 User 가 아니다. 우리는 Member 사용할 예정.
+
+        UserDetails userDetails = User.builder()
+                .username("lsy")
+                .password(passwordEncoder.encode("123456"))
+                .authorities("ROLE_USER")
+                .build();
+
+        return userDetails;
     }
 }
