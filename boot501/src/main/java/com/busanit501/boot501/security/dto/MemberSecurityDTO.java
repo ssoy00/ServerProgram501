811df596bc5b2fadeee985b17f1ef11c6e9d4973
이상용 <lsy3709@naver.com>
2024-06-20 11:30:13 +0900

    3교시 시큐리티 타입에 맞는 DTO로 만들기. MemberSecurityDTO

---
+++ boot501/src/main/java/com/busanit501/boot501/security/dto/MemberSecurityDTO.java
@@ -0,0 +1,41 @@
+package com.busanit501.boot501.security.dto;
+
+//시큐리티 필터 설정이 되어 있고,
+// 로그인 처리를 우리가 하는게 아니라, 시큐리티가 함.
+// 시큐리티는 그냥 클래스를 요구하지 않고,
+// 자기들이 정해둔 룰. UserDetails 를 반환하는 클래스를 요구를 해요.
+// 시큐리티에서 정의해둔 특정 클래스를 상속을 받으면 됨.
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Data
+//@AllArgsConstructor
+// @AllArgsConstructor 대신에 권한도, 시큐리티에서 가져와서, 사용자정의해야하서.
+public class MemberSecurityDTO extends User {
+    private String mid;
+    private String mpw;
+    private String email;
+    private boolean del;
+    private boolean social;
+
+    //생성자
+    public MemberSecurityDTO(
+            //로그인한 유저이름.
+            String username,String password,String email,
+            boolean del, boolean social,
+            //GrantedAuthority 를 상속한 클래스는 아무나 올수 있다. 타입으로
+            Collection<? extends GrantedAuthority> authorities
+    ){
+      super(username, password, authorities);
+      this.mid = username;
+      this.mpw = password;
+      this.email = email;
+      this.del = del;
+      this.social = social;
+    }
+}
