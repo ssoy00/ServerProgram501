969d24153481a016ae7c002220ac6cebe15b3c50
이상용 <lsy3709@naver.com>
2024-06-20 10:16:27 +0900

    2교시 로그인 화면에서 확인 해보기2

--- boot501/src/main/java/com/busanit501/boot501/controller/MemberController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/MemberController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/member")
@@ -12,12 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MemberController {
     @GetMapping("/login")
-    public void loginGet(String error, String logout) {
+    public void loginGet(String error, String logout,
+                         RedirectAttributes redirectAttributes) {
         log.info("loginGet===================");
         log.info("logoutTest ===================" + logout);
 
         if(logout != null) {
             log.info("logoutTest user ====================");
+        }
+        if(error != null) {
+            log.info("logoutTest error ====================" + error);
+            redirectAttributes.addFlashAttribute(
+                    "error", "");
         }
 
     }
