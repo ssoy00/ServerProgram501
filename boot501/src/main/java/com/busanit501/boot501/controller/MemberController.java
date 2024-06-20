ae91f3cb131c2775563cb68c99930c95331d0baf
이상용 <lsy3709@naver.com>
2024-06-20 12:36:09 +0900

    4교시 회원가입 로직처리하기 , 컨트롤러에 연결하기

--- boot501/src/main/java/com/busanit501/boot501/controller/MemberController.java
+++ boot501/src/main/java/com/busanit501/boot501/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.busanit501.boot501.controller;
 
 import com.busanit501.boot501.dto.MemberJoinDTO;
+import com.busanit501.boot501.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
@@ -14,16 +15,18 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Log4j2
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
     @GetMapping("/login")
     public void loginGet(String error, String logout,
                          RedirectAttributes redirectAttributes) {
         log.info("loginGet===================");
         log.info("logoutTest ===================" + logout);
 
-        if(logout != null) {
+        if (logout != null) {
             log.info("logoutTest user ====================");
         }
-        if(error != null) {
+        if (error != null) {
             log.info("logoutTest error ====================" + error);
             redirectAttributes.addFlashAttribute(
                     "error", "");
@@ -42,13 +45,20 @@ public class MemberController {
 
     // 회원 가입 로직 처리
     @PostMapping("/join")
-    public String joinPost(MemberJoinDTO memberJoinDTO) {
+    public String joinPost(MemberJoinDTO memberJoinDTO,
+                           RedirectAttributes redirectAttributes) {
         log.info("joinPost====================");
         log.info("memberJoinDTO = " + memberJoinDTO);
         // 회원 가입 로직 처리 없음.
-
-
-        return "redirect:/board/list";
+        try {
+            memberService.join(memberJoinDTO);
+        } catch (MemberService.MidExistException e) {
+            redirectAttributes.addFlashAttribute("error", "아이디 중복입니다")
+            return "redirect:/member/join";
+        }
+        // 회원 가입 성공시
+        redirectAttributes.addFlashAttribute("result","회원가입 성공");
+        return "redirect:/member/login";
     }
 
 }
