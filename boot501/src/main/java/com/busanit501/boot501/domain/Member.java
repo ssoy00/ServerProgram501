c77b699fc61cc359fd8a866e15797a8bf18b4e90
이상용 <lsy3709@naver.com>
2024-06-21 10:45:16 +0900

    2교시, 프로필 이미지 추가 화면작업, 이미지 업로드 확인 테스트

--- boot501/src/main/java/com/busanit501/boot501/domain/Member.java
+++ boot501/src/main/java/com/busanit501/boot501/domain/Member.java
@@ -25,6 +25,11 @@ public class Member extends  BaseEntity {
 
     private boolean social;
 
+    //이미지 파일명 필요해서,
+    // 프로필 이미지 조회시 사용.
+    private String uuid;
+    private String fileName;
+
     // 멤버를 조회시 roleSet 를 같이 조회를 하기.
     @ElementCollection(fetch = FetchType.LAZY)
     @Builder.Default
--- boot501/src/main/java/com/busanit501/boot501/dto/MemberJoinDTO.java
+++ boot501/src/main/java/com/busanit501/boot501/dto/MemberJoinDTO.java
@@ -1,7 +1,6 @@
 package com.busanit501.boot501.dto;
 
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class MemberJoinDTO {
@@ -10,6 +9,5 @@ public class MemberJoinDTO {
     private String email;
     private boolean del;
     private boolean social;
-    //프로필 이미지
-    private MultipartFile file;
+
 }
