97bb2db0c5e046e5523ade11432e6a962b8eae2e
이상용 <lsy3709@naver.com>
2024-06-27 11:39:25 +0900

    shop, 이식 작업중9, board 멤버, shop 멤버 합치기 작업6, 수정중1

--- boot501/src/main/java/com/busanit501/boot501/service/MemberServiceImpl.java
+++ boot501/src/main/java/com/busanit501/boot501/service/MemberServiceImpl.java
@@ -37,8 +37,8 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public boolean checkMid(String mid) {
-        boolean existMember = memberRepository.existsById(mid);
-        if (existMember) {
+        Optional<Member> result = memberRepository.findByMid(mid);
+        if (result.isPresent()) {
             return true;
         } else {
             return false;
@@ -49,8 +49,9 @@ public class MemberServiceImpl implements MemberService {
     public void join(MemberJoinDTO memberJoinDTO) throws MidExistException {
         //기존 아이디와 중복되는지 여부 확인
         String mid = memberJoinDTO.getMid();
-        boolean existMember = memberRepository.existsById(mid);
-        if (existMember) {
+        Optional<Member> result = memberRepository.findByMid(mid);
+
+        if (result.isPresent()) {
             throw new MidExistException();
         }
 
@@ -84,7 +85,7 @@ public class MemberServiceImpl implements MemberService {
         if(memberJoinDTO.getUuid() == null) {
             // 기존 이미지 재사용.
             log.info("memberJoinDTO = 4-2 MemberServiceImpl 기존 이미지 재사용  " + memberJoinDTO);
-            Optional<Member> result = memberRepository.findById(memberJoinDTO.getMid());
+            Optional<Member> result = memberRepository.findByMid(memberJoinDTO.getMid());
             Member oldMember = result.orElseThrow();
             memberJoinDTO.setUuid(oldMember.getUuid());
             memberJoinDTO.setFileName(oldMember.getFileName());
