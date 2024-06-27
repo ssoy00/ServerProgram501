0edc4d62e90e370a37ccce5949da7aa1f3ee992e
이상용 <lsy3709@naver.com>
2024-06-27 11:22:45 +0900

    shop, 이식 작업중9, board 멤버, shop 멤버 합치기 작업4

--- boot501/src/main/java/com/busanit501/boot501/shop/repository/ShopMemberRepository.java
+++ boot501/src/main/java/com/busanit501/boot501/shop/repository/ShopMemberRepository.java
@@ -1,11 +1,8 @@
 package com.busanit501.boot501.shop.repository;
 
 
-import com.busanit501.boot501.shop.entity.ShopMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopMemberRepository extends JpaRepository<ShopMember, Long> {
-
-    ShopMember findByEmail(String email);
-
-}
+//public interface ShopMemberRepository extends JpaRepository<ShopMember, Long> {
+//
+//    ShopMember findByEmail(String email);
+//
+//}
