4ba15884043e5c9758f5f7cc8485bc3c8ca665f8
이상용 <lsy3709@naver.com>
2024-06-27 11:28:43 +0900

    shop, 이식 작업중9, board 멤버, shop 멤버 합치기 작업5, 1차 확인

--- boot501/src/main/java/com/busanit501/boot501/shop/repository/OrderRepository.java
+++ boot501/src/main/java/com/busanit501/boot501/shop/repository/OrderRepository.java
@@ -1,6 +1,5 @@
 package com.busanit501.boot501.shop.repository;
 
-
 import com.busanit501.boot501.shop.entity.Order;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,13 +11,13 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select o from Order o " +
-            "where o.shopMember.email = :email " +
+            "where o.member.email = :email " +
             "order by o.orderDate desc"
     )
     List<Order> findOrders(@Param("email") String email, Pageable pageable);
 
     @Query("select count(o) from Order o " +
-            "where o.shopMember.email = :email"
+            "where o.member.email = :email"
     )
     Long countOrder(@Param("email") String email);
 }
